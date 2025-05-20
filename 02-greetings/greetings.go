fa18780e39b3bda8be377eca8c0af024dc54e26e
milosdjurica <milosdjurica.work@gmail.com>
2025-05-20 16:43:38 +0200

    error handling

--- 02-greetings/greetings.go
+++ 02-greetings/greetings.go
@@ -1,12 +1,19 @@
 package greetings
 
-import "fmt"
+import (
+	"errors"
+	"fmt"
+)
 
-func Hello(name string) string {
+func Hello(name string) (string, error) {
+	if name == "" {
+		return "", errors.New("empty name")
+	}
+
 	// Return a greeting that embeds the name in a message.
 	message := fmt.Sprintf("Hi, %v. Welcome!", name)
 	// Upper code is shortcut for 2 lines below
 	// var message string
 	// message = fmt.Sprintf("Hi, %v. Welcome!", name)
-	return message
+	return message, nil
 }
--- 03-hello-call/hello-call.go
+++ 03-hello-call/hello-call.go
@@ -2,11 +2,21 @@ package main
 
 import (
 	"fmt"
+	"log"
 
 	greetings "github.com/milosdjurica/go-practice/02-greetings"
 )
 
 func main() {
-	message := greetings.Hello("Milos")
+
+	log.SetPrefix("greetings: ")
+	log.SetFlags(0)
+
+	message, err := greetings.Hello("Milos")
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
 	fmt.Println(message)
 }
